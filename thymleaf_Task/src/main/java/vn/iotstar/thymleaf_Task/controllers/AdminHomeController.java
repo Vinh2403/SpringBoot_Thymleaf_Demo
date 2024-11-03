c56e71957c97b5a15519cc1bb040f1d54bd6439e
Vinh2403 <phamv4356@gmail.com>
2024-11-03 15:10:38 +0700

    Sửa lỗi hiển thị i18n và hoàn thành Task!

--- thymleaf_Task/src/main/java/vn/iotstar/thymleaf_Task/controllers/AdminHomeController.java
+++ thymleaf_Task/src/main/java/vn/iotstar/thymleaf_Task/controllers/AdminHomeController.java
@@ -1,5 +1,7 @@
 package vn.iotstar.thymleaf_Task.controllers;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,7 @@ public class AdminHomeController {
 	
 	@GetMapping("/home")
 	public String index() {
+		System.out.println("Current Locale: " + LocaleContextHolder.getLocale());
 		return "admin/layout_admin";
 	}
 }
